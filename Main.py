97f516d3a04e33082c2a79101192034a5d5d05e3
Hamza-Abbas-2007 <hamzaspt7@gmail.com>
2025-03-25 01:13:48 +0200

    Fetches temperature

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import requests
+import json
 
 API_KEY = 'PM66FQJM5T8WARQP2K4NH8UVM'
 url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cairo?unitGroup=metric&include=days&key={API_KEY}"
 response = requests.get(url)
 print(response.status_code)
+jsonData = response.json()
+
+info = jsonData["days"][0]
+temp = info["temp"]
+print(temp) 
