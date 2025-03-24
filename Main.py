d5f94e0e05455c22a903d69f8c7b79c96c0090b0
Hamza-Abbas-2007 <hamzaspt7@gmail.com>
2025-03-25 00:51:39 +0200

    api test

--- Main.py
+++ Main.py
@@ -1,1 +1,6 @@
-print("hello world")
+import requests
+
+API_KEY = 'PM66FQJM5T8WARQP2K4NH8UVM'
+url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cairo?unitGroup=metric&include=days&key={API_KEY}"
+response = requests.get(url)
+print(response.status_code)
