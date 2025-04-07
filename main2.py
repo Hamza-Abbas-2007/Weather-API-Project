a5d9c396ba928b9b71b1193b193dfa288270ef39
Hamza-Abbas-2007 <hamzaspt7@gmail.com>
2025-04-07 22:59:03 +0200

    Adding Humidity

--- main2.py
+++ main2.py
@@ -4,17 +4,20 @@ from datetime import datetime
 import requests
 from diskcache import Cache
 import sys
+from flask_migrate import Migrate
 sys.stdout.write("Hello")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 cache = Cache('./cache_directory')
+migrate = Migrate(app, db)
 
 class todo (db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
     city_name = db.Column(db.String(200), nullable = False)
+    Humidity = db.Column(db.String(200), nullable = True)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
     
     def __repr__(self):
@@ -39,14 +42,33 @@ def Get_weather(city):
         cache.set(cache_key, temp, expire=60)
         print(f"Cached successfully for {city}")
         return temp
+    
+def Get_Humidity(city):
+    cache_key = f'Humidity_{city}'
+    
+    if cache_key in cache:
+        print("Cache hit: Returning cached Humidity")
+        return cache[cache_key]
+
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&include=days&key={API_KEY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        jsonData = response.json()
+        humidity = jsonData["days"][0]["humidity"]
+        
+        cache.set(cache_key, humidity , expire=60)
+        print(f"Cached Humidity successfully for {city}")
+        return humidity
 
 @app.route('/' ,methods = ['post','get'])
 def index():
     city = request.form.get('city')
-    cachedtemp = Get_weather(city) 
+    cachedtemp = Get_weather(city)
+    cachedHumidity = Get_Humidity(city) 
     if request.method == 'POST':
     
-        new_entry = todo(content=str(cachedtemp), city_name=str(city))
+        new_entry = todo(content=str(cachedtemp), city_name=str(city), Humidity = str(cachedHumidity))
         try:
             if todo.query.count() < 10:
                 db.session.add(new_entry)
